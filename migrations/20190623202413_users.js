549d08aa16ddbdae1278526f0dcbb707210955c7
coriagogo <pkcparis@gmail.com>
2019-06-23 20:51:33 -0400

    database and migrations changed/updated

--- migrations/20190623202413_users.js
+++ migrations/20190623202413_users.js
@@ -1,19 +1,23 @@
-
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('users', users => {
     users.increments();
-
+ 
     users
       .string('username', 128)
       .notNullable()
       .unique();
-
+ 
     users
       .string('password', 128)
-      .notNullable();   
+      .notNullable(); 
+     
+    users
+      .string('name', 255)
+      .notNullable();
   }
-)};
-
-exports.down = function(knex, Promise) {
+ )};
+ 
+ exports.down = function(knex, Promise) {
   return knex.schema.dropTableIfExists('users');
-};
+ };
+ 
