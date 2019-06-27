e18a94bf9467b244dbe0c983e86e567ede03d599
coriagogo <pkcparis@gmail.com>
2019-06-26 20:21:52 -0400

    updated dbconfig

--- data/dbConfig.js
+++ data/dbConfig.js
@@ -2,6 +2,6 @@ const knex = require('knex');
 
 const knexConfig = require('../knexfile.js');
 
-const environment = process.env.DATABASE_URL || 'development';
+const environment = process.env.DB_ENV || 'development';
 
 module.exports = knex(knexConfig[environment]);
